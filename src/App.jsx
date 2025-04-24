1629e5217073b6a27e81908b23a14a14257924b3
Narendra Chowdary <narendrabollineni2002@gmail.com>
2025-04-24 13:38:06 +0000

    Update homepage URL and user information in App component

--- src/App.jsx
+++ src/App.jsx
@@ -24,10 +24,10 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Bollineni Narendra Chowdary",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  email: "narendrabollineni2002@gmail.com",
+  gitHub: "nrenx",
   instagram: "microsoft",
   linkedIn: "satyanadella",
   medium: "",
